e847bf3830a2348b9c4f832d5bbd854955334fe4
Paweł Tomulik <pawel@tomulik.pl>
2026-03-27 11:59:09 +0100

    enabled BC checks in util/run-checks.sh

--- util/run-checks.sh
+++ util/run-checks.sh
@@ -18,6 +18,11 @@ error() {
   echo -e "\033[0;31merror: $@\033[0m" >&2
 }
 
+execute() {
+  echo -e "\033[0;92m$@\033[0m";
+  "$@"
+}
+
 check_result() {
     if [[ $1 -ne 0 ]]; then
         error "check failed with status code: $1"
@@ -27,36 +32,43 @@ check_result() {
     fi
 }
 
+php=php
+php_version=$($php -r 'printf("%s.%s", PHP_MAJOR_VERSION, PHP_MINOR_VERSION);');
+psalm_baseline=".psalm/baseline-php${php_version}.xml"
 
 pushd $top > /dev/null 2>&1
     info "running phpunit"
-    vendor-bin/phpunit/vendor/bin/phpunit
+    execute $php vendor-bin/phpunit/vendor/bin/phpunit
     check_result $?;
     echo "";
 
     echo "";
     info "running vimeo/psalm"
-    vendor-bin/psalm/vendor/bin/psalm --no-progress --no-cache --show-info=true --stats
+    if [[ -f  $psalm_baseline ]]; then
+        execute $php vendor-bin/psalm/vendor/bin/psalm --no-progress --no-cache --show-info=true --stats --php-version="$php_version" --use-baseline="$psalm_baseline"
+    else
+        execute $php vendor-bin/psalm/vendor/bin/psalm --no-progress --no-cache --show-info=true --stats --php-version="$php_version"
+    fi
     check_result $?;
     echo "";
 
     echo "";
     info "runnig php-cs-fixer"
-    vendor-bin/php-cs-fixer/vendor/bin/php-cs-fixer fix --diff --dry-run --show-progress=dots --using-cache=no --verbose
+    execute $php vendor-bin/php-cs-fixer/vendor/bin/php-cs-fixer fix --diff --dry-run --show-progress=dots --using-cache=no --verbose
     check_result $?;
     echo "";
 
     echo "";
     info "runnig composer-require-checker"
-    vendor-bin/composer-require-checker/vendor/bin/composer-require-checker check
+    execute $php vendor-bin/composer-require-checker/vendor/bin/composer-require-checker check
     check_result $?;
     echo "";
 
-    ## echo "";
-    ## info "runnig roave-backward-compatibility-check"
-    ## vendor-bin/backward-compatibility-check/vendor/bin/roave-backward-compatibility-check
-    ## check_result $?
-    ### echo "";
+    echo "";
+    info "runnig roave-backward-compatibility-check"
+    execute $php vendor-bin/backward-compatibility-check/vendor/bin/roave-backward-compatibility-check
+    check_result $?
+    echo "";
 popd >/dev/null 2>&1
 
 if [[ $failed -eq 1 ]]; then
